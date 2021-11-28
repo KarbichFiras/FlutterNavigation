4412b6968a9dd6e31e092b0cd44077367e302c54
KarbichFiras <firaskarbich@gmail.com>
2021-11-28 13:27:17 +0100

    added the 2nd button

--- pass_argument/lib/main.dart
+++ pass_argument/lib/main.dart
@@ -153,6 +153,27 @@ class _MyHomePageState extends State<MyHomePage> {
               },
               child: const Text('Navigate to screen that extracts arguments'),
             ),
+            // A button that navigates to a named route.
+            // For this route, extract the arguments in
+            // the onGenerateRoute function and pass them
+            // to the screen.
+            ElevatedButton(
+              onPressed: () {
+                // When the user taps the button, navigate
+                // to a named route and provide the arguments
+                // as an optional parameter.
+                Navigator.pushNamed(
+                  context,
+                  PassArgumentsScreen.routeName,
+                  arguments: ScreenArguments(
+                    'Accept Arguments Screen',
+                    'This message is extracted in the onGenerateRoute '
+                        'function.',
+                  ),
+                );
+              },
+              child: const Text('Navigate to a named that accepts arguments'),
+            ),
           ],
         ),
       ),
